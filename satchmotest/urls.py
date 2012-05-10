c1e8ab9d8d74a4385b638432c9fb8351d7589fe5
Simone Deponti <simone.deponti@gmail.com>
2012-05-10 12:23:10 +0200

    Added static serving

--- satchmotest/urls.py
+++ satchmotest/urls.py
@@ -1,5 +1,20 @@
-from django.conf.urls.defaults import *
+from django.conf import settings
+from django.conf.urls.defaults import patterns, url
 from satchmo_store.urls import urlpatterns as storepatterns
 from l10n.urls import urlpatterns as l10npatterns
 
+
 urlpatterns = storepatterns + l10npatterns
+
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        '',
+        url(
+            r'^%s/(?P<path>.*)$' % settings.STATIC_URL.strip('/'),
+            'django.views.static.serve',
+            {
+                'document_root': settings.STATIC_ROOT,
+            }
+        )
+    )
